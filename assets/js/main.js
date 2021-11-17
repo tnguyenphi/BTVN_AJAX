f1ae77419529660e7bbb56680fa6db5c52d72bf3
NguyenPhi <nguyenphidhbk@gmail.com>
2021-11-17 11:20:18 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,18 +22,20 @@ getInfo();
 function renderData(data){
     var html = "";
     data.forEach(function(item){
-        html += `
-        <div class="col-6 col-lg-3">
-        <div class="card">
-            <img src="./img/${item.hinhAnh}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5>${item.ngonNgu}</h5>
-                <h3 class="card-title">${item.hoTen}</h3>
-                <p class="card-text">${item.moTa}t</p>
+        if (item.loaiND == "GV"){
+            html += `
+            <div class="col-6 col-lg-3">
+            <div class="card">
+                <img src="./img/${item.hinhAnh}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5>${item.ngonNgu}</h5>
+                    <h3 class="card-title">${item.hoTen}</h3>
+                    <p class="card-text">${item.moTa}t</p>
+                </div>
             </div>
         </div>
-    </div>
-        `
+            `
+        }
     })
     getEle("card_content").innerHTML = html;
 }
